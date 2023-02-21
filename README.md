d03a675d65a5cdc125c5c1dd873b3ff2244d01d5
Jake <jakebayley01@gmail.com>
2023-02-21 17:11:46 +0000

    Completed game, with help from youtube tutorial.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Rock-Paper-Scissors
 My first js project
 
+What I want to make is a game that lets me play rock paper scissors against the computer:
+1. I will enter a value of either rock, paper or scissors into the machine.
+2. The machine will randomly choose rock, paper or scissors themselves.
+3. The computer should then take these two values and compare them where Scissors > Paper > Rock > Scissors
+4. When this is completed, the computer will tell the user whether the computer won, the user won, or it was in fact a draw (if both players chose the same variable)
--- javascript.js
+++ javascript.js
@@ -1,3 +1,73 @@
-const ComputerChoice = ["rock", "paper", "scissors"];
-const getComputerChoice = ComputerChoice[Math.floor(Math.random() * ComputerChoice.length)];
-console.log(getComputerChoice)
+const choices = ['rock', 'paper', 'scissors'];
+
+function game(){
+    for (let i = 0; i <= 4; i++)
+    playRound();
+};
+
+function playRound(){
+    const playerSelection= playerChoice();
+    const comp = compChoice();
+    const winner = checkWinner(playerSelection,comp)
+    console.log(winner)
+
+};
+
+function playerChoice () {
+    let input = prompt('Choose either rock, paper or scissors.');
+    while (input == null){
+        input = prompt('Choose either rock, paper or scissors.');
+
+    }
+
+
+    input = input.toLowerCase();
+
+    let check = validateInput(input);
+     while(check == false){
+        input = prompt('Choose either rock, paper or scissors. Spelling needs to be exact.');
+
+        while (input == null){
+            input = prompt('Choose either rock, paper or scissors. Spelling needs to be exact.');
+    
+        };
+        input = input.toLowerCase();
+        check = validateInput(input);
+
+        
+     }
+
+     return input;
+
+    
+
+    
+     }
+
+function compChoice(){
+    return choices[Math.floor(Math.random()*3)];
+};
+
+function validateInput(choice){
+    if (choices.includes(choice)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function checkWinner(ChoiceP,ChoiceC){
+    if (ChoiceC === ChoiceP){
+        return 'This is a tie!';
+
+    }
+    else if ((ChoiceC === 'rock') && (ChoiceP === 'scissors') || ((ChoiceC === 'scissors') && (ChoiceP === 'paper')) || ((ChoiceC === 'paper') && (ChoiceP === 'rock'))){
+        return'You lose!';
+
+    }
+    else{
+        return'You win!';
+    };
+};
+game()
