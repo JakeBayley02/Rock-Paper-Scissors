37139f3d1c502bd081f196edbf90ec5d4078d9ac
Jake <jakebayley01@gmail.com>
2023-03-10 17:15:04 +0000

    All finished

--- javascript.js
+++ javascript.js
@@ -1,26 +1,63 @@
+let winners = [];
 const choices = ['rock', 'paper', 'scissors'];
-/*
+
+function resetGame(){
+    winners = [];
+    document.querySelector('#player').textContent = '';
+    document.querySelector('#computer').textContent = '';
+    document.querySelector('#ties').textContent = '';
+    document.querySelector('#winner').textContent = '';
+    
+}
+
 function game(){
-    for (let i = 0; i <= 4; i++)
-    playRound();
-};
-*/
-
-const imgs = document.querySelectorAll('img');
-
-
-imgs.forEach((img) => 
+    imgs.forEach((img) => 
    img.addEventListener('click', () =>
    playRound(img.id))
 );
 
+};
+
+const body = document.querySelector('body');
+const imgs = document.querySelectorAll('img');
+const results = document.querySelector('#results');
+
+
+
 function playRound(playerSelection){
+    let wins = checkWins();
+    if (wins >= 5){
+        return;
+    };
+
     const comp = compChoice();
     const winner = checkWinner(playerSelection,comp)
-    console.log(winner, comp, playerSelection)
 
-
+    winners.push(winner);
+    
+    tallyWins();
+    wins = checkWins();
+    if (wins == 5){
+        endGame();
+    };
+    
 };
+
+function endGame(){
+    document.querySelector('#player').textContent = '';
+    document.querySelector('#computer').textContent = '';
+    document.querySelector('#ties').textContent = '';
+    computerWins = winners.filter((item) => item == 'Computer wins').length;
+    if (computerWins === 5){
+        document.querySelector('#winner').textContent = 'Unlucky, the computer wins the game!';
+    }
+
+    else{
+        document.querySelector('#winner').textContent = 'Congrats! You won the game!';
+
+    };
+    document.querySelector('.Restart').style.display = 'flex';
+}
 
 function compChoice(){
     return choices[Math.floor(Math.random()*3)];
@@ -28,15 +65,34 @@ function compChoice(){
 
 function checkWinner(ChoiceP,ChoiceC){
     if (ChoiceC === ChoiceP){
-        return 'This is a tie!';
+        return 'Tie';
 
     }
     else if ((ChoiceC === 'rock') && (ChoiceP === 'scissors') || ((ChoiceC === 'scissors') && (ChoiceP === 'paper')) || ((ChoiceC === 'paper') && (ChoiceP === 'rock'))){
-        return'You lose!';
+        return'Computer wins';
 
     }
     else{
-        return'You win!';
+        return'Player wins';
     };
 };
-playRound();
+
+function checkWins(){
+    const cWins = winners.filter((item) => item == 'Computer wins').length;
+    const pWins = winners.filter((item) => item == 'Player wins').length;
+    return Math.max(cWins,pWins);
+};
+
+
+function tallyWins(){
+    const cTallyWins = winners.filter((item) => item == 'Computer wins').length;
+    const pTallyWins = winners.filter((item) => item == 'Player wins').length;
+    const tallyTies = winners.filter((item) => item == 'Tie').length;
+    document.querySelector('#player').textContent = `Player wins: ${pTallyWins}`;
+    document.querySelector('#computer').textContent = `Computer wins: ${cTallyWins}`;
+    document.querySelector('#ties').textContent = `Ties: ${tallyTies}`;
+
+};
+
+
+game();
